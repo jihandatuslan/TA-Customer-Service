b6d73e6e8a58aab8b48dfc503efcfc58a4e88fcd
jihan datuslan <jihandatuslanibrahim@gmail.com>
2025-06-12 12:20:34 +0700

    Integrasi customer service dengan registry service

--- src/main/java/com/tugas_akhir/customer_service/CustomerServiceApplication.java
+++ src/main/java/com/tugas_akhir/customer_service/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.tugas_akhir.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //untuk registrasi ke service discovery
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
